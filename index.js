39fe404b163414ea5528f5a736505192d6e4bd0d
Matt Beckstead <mattbeckstead@gmail.com>
2014-08-09 21:19:12 +0000

    modified index.js to read index.html to use as output.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var buf = require('buffer');
+var fs = require('fs');
 var app = express();
-var outputString = "Testing outputString";
+
+var outputString = "Error: value of outputString was never set.";
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-// outputString = buf.toString(fs.readFileSync("index.html"));
+outputString = buf.toString(fs.readFileSync("index.html"));
 
 
 app.get('/', function(request, response) {
