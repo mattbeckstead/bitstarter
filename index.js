9cd2599d1abb03276c9b0b8d94bb9e72cbe37c5c
Matt Beckstead <mattbeckstead@gmail.com>
2014-08-09 21:11:26 +0000

    Actually adding modified index.js file this time.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var outputString = "Testing outputString";
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+// outputString = buf.toString(fs.readFileSync("index.html"));
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send(outputString);
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
